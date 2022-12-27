214022c8bae0a50c422c094d317d5ff72affa145
CitySefti <71775364+CitySefti@users.noreply.github.com>
2022-12-27 14:57:03 +0000

    comments
    revised comments
    
    added comments

--- GridTraversal/main.py
+++ GridTraversal/main.py
@@ -7,7 +7,6 @@ maxVal = 10
 map = np.random.randint(1, maxVal, size = (maxVal, maxVal))
 
 # Initialize auxiliary arrays
-
 distMap = np.ones((maxVal, maxVal), dtype = int) * np.Infinity
 distMap[0, 0] = 0
 originMap = np.ones((maxVal, maxVal), dtype = int) * np.nan
@@ -16,8 +15,7 @@ finished = False
 x, y = int(0), int(0)
 count = 0
 
-# Loop Dijkstra until reaching the target cell
-
+# Loop until reaching the target cell
 while not finished:
     # Checks the adjacent nodes to the current one and moves to the smallest in the direction of the goal.
     if x < maxVal - 1:
@@ -52,13 +50,13 @@ while not finished:
         finished = True
     count = count + 1
 
-# Start backtracking to plot the path
-
+# Auxiliary map
 mapTemp = map.astype(float)
 x, y = maxVal - 1, maxVal - 1
 path = []
 mapTemp[int(x), int(y)] = np.nan
 
+# Backtrack to plot path
 while x > 0.0 or y > 0.0:
     path.append([int(x), int(y)])
     xxyy = np.unravel_index(int(originMap[int(x), int(y)]), (maxVal, maxVal))
@@ -66,8 +64,7 @@ while x > 0.0 or y > 0.0:
     mapTemp[int(x), int(y)] = np.nan
     path.append([int(x), int(y)])
 
-# Output and visualization of the path
-
+# Visualize and output
 cmap = mpl.cm.get_cmap("binary").copy() # I get an error without mpl....copy()
 cmap.set_bad(color = 'purple')
 fig, plot = plt.subplots(figsize = (10, 10))
@@ -78,6 +75,4 @@ for i in range(maxVal):
         plot.text(i, j, str(c), va = 'center', ha = 'center')
 
 print('Path Length = ' + str(distMap[maxVal - 1, maxVal - 1]))
-print('Mean Path Length = ' + str(maxVal * maxVal))
-
 plt.show()
