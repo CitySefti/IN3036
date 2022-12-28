92683b659bb5880f728c0bc656cbee270e8eb3be
CitySefti <71775364+CitySefti@users.noreply.github.com>
2022-12-28 15:42:55 +0000

    reformat
    removed the notifications in the "Problems" tab (nothing major, just format and spelling)

--- GridTraversal/main.py
+++ GridTraversal/main.py
@@ -1,17 +1,19 @@
 from tkinter import *
 from subprocess import call
 
+
 def click():
     call(["python", "traverse.py"])
+
 
 window = Tk(className="Task 1")
 window.geometry("300x300")
 window.configure(bg="black")
 
-button = Button(window, text="Traverse", command=click, font="Arial", fg="black", bg="white", activeforeground="white", activebackground="white")
-button.place(x=150, y = 125)
+button = Button(window, text="Traverse", command=click, font="Arial", fg="black", bg="white", activeforeground="white",
+                activebackground="white")
+button.place(x=150, y=125)
 button.pack()
 
-# idea is to add other buttons for other traverse options (e.g with obstacles)
+# idea is to add other buttons for other traverse options (e.g. with obstacles)
 window.mainloop()
-
--- GridTraversal/traverse.py
+++ GridTraversal/traverse.py
@@ -4,13 +4,13 @@ import matplotlib as mpl
 
 minVal = 0
 maxVal = 10
-map = np.random.randint(1, maxVal, size = (maxVal, maxVal))
+map = np.random.randint(1, maxVal, size=(maxVal, maxVal))
 
 # Initialize auxiliary arrays
-distMap = np.ones((maxVal, maxVal), dtype = int) * np.Infinity
+distMap = np.ones((maxVal, maxVal), dtype=int) * np.Infinity
 distMap[0, 0] = 0
-originMap = np.ones((maxVal, maxVal), dtype = int) * np.nan
-visited = np.zeros((maxVal, maxVal), dtype = bool)
+originMap = np.ones((maxVal, maxVal), dtype=int) * np.nan
+visited = np.zeros((maxVal, maxVal), dtype=bool)
 finished = False
 x, y = int(0), int(0)
 count = 0
@@ -39,12 +39,12 @@ while not finished:
             originMap[x, y - 1] = np.ravel_multi_index([x, y], (maxVal, maxVal))
 
     visited[x, y] = True
-    distmapTemp = distMap
-    distmapTemp[np.where(visited)] = np.Infinity
+    distMapTemp = distMap
+    distMapTemp[np.where(visited)] = np.Infinity
 
     # now we find the shortest path so far
 
-    minPos = np.unravel_index(np.argmin(distmapTemp), np.shape(distmapTemp))
+    minPos = np.unravel_index(np.argmin(distMapTemp), np.shape(distMapTemp))
     x, y = minPos[0], minPos[1]
     if x == maxVal - 1 and y == maxVal - 1:
         finished = True
@@ -65,14 +65,14 @@ while x > 0.0 or y > 0.0:
     path.append([int(x), int(y)])
 
 # Visualize and output
-cmap = mpl.cm.get_cmap("binary").copy() # I get an error without mpl....copy()
-cmap.set_bad(color = 'purple')
-fig, plot = plt.subplots(figsize = (10, 10))
-plot.matshow(mapTemp, cmap = cmap, vmin = 0, vmax = 15)
+cMap = mpl.cm.get_cmap("binary").copy()  # I get an error without mpl....copy()
+cMap.set_bad(color='purple')
+fig, plot = plt.subplots(figsize=(10, 10))
+plot.matshow(mapTemp, cmap=cMap, vmin=0, vmax=15)
 for i in range(maxVal):
     for j in range(maxVal):
         c = map[j, i]
-        plot.text(i, j, str(c), va = 'center', ha = 'center')
+        plot.text(i, j, str(c), va='center', ha='center')
 
 print('Path Length = ' + str(distMap[maxVal - 1, maxVal - 1]))
 plt.show()
