6c12aab19d1741920e526feb5d0005d1a9686f07
CitySefti <71775364+CitySefti@users.noreply.github.com>
2022-12-29 22:09:58 +0000

    Parameterised grid traversal
    used this as help for form:
    
    https://www.javatpoint.com/simple-registration-form-using-tkinter-in-python

--- GridTraversal/main.py
+++ GridTraversal/main.py
@@ -1,18 +1,56 @@
 from tkinter import *
 from subprocess import call
+from traverse import Traverse
+
+"""
+def click():
+    call(["python", "traverse.py"])
+"""
+window = Tk(className="Task 1")
+window.geometry("350x350")
+
+minLabel = Label(window, text="Minimum")
+minLabel.place(x=80, y=50)
+cellMinInput = Entry(window)
+cellMinInput.place(x=180, y=50)
+
+maxLabel = Label(window, text="Maximum")
+maxLabel.place(x=80, y=100)
+cellMaxInput = Entry(window)
+cellMaxInput.place(x=180, y=100)
+
+widthLabel = Label(window, text="Width")
+widthLabel.place(x=80, y=150)
+widthInput = Entry(window)
+widthInput.place(x=180, y=150)
+
+heightLabel = Label(window, text="Height")
+heightLabel.place(x=80, y=200)
+heightInput = Entry(window)
+heightInput.place(x=180, y=200)
 
 
 def click():
-    call(["python", "traverse.py"])
+    if cellMinInput.get() != 0:
+        min = int(cellMinInput.get())
 
+    if cellMaxInput.get() != 0:
+        max = int(cellMaxInput.get())
 
-window = Tk(className="Task 1")
-window.geometry("300x300")
-window.configure(bg="black")
+    if cellMinInput.get() != 0:
+        width = int(widthInput.get())
 
-button = Button(window, text="Traverse", command=click, font="Arial", fg="black", bg="white", activeforeground="white",
-                activebackground="white")
-button.place(relx=0.5, rely=0.5, anchor=CENTER)
+    if cellMinInput.get() != 0:
+        height = int(heightInput.get())
+
+    newAgent = Traverse(cellMin=min, cellMax=max, gridWidth=width, gridHeight=height)
+    newAgent.run()
+
+button = Button(window, text="Traverse", command=click, font="Arial")
+button.place(x=130, y=250)
 
 # idea is to add other buttons for other traverse options (e.g. with obstacles)
 window.mainloop()
+
+#hello = Traverse(cellMin=0, cellMax=10, gridWidth=10, gridHeight=10)
+#hello.run()
--- GridTraversal/traverse.py
+++ GridTraversal/traverse.py
@@ -1,78 +1,102 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+import time
 
-minVal = 0
-maxVal = 10
-map = np.random.randint(minVal, maxVal, size=(maxVal, maxVal))
+class Traverse:
 
-# Auxiliary arrays and variables
-distMap = np.ones((maxVal, maxVal), dtype=int) * np.Infinity
-distMap[0, 0] = 0
-originMap = np.ones((maxVal, maxVal), dtype=int) * np.nan
-visited = np.zeros((maxVal, maxVal), dtype=bool)
-finished = False
-xPos, yPos = int(0), int(0)
-count = 0
+    def __init__(self, cellMin, cellMax, gridWidth, gridHeight):
+        self.cellMin = cellMin
+        self.cellMax = cellMax
+        self.gridWidth = gridWidth
+        self.gridHeight = gridHeight
 
-# Loop until reaching the target cell
-while not finished:
-    # Checks the adjacent nodes to the current one and moves to the smallest in the direction of the goal.
-    if xPos < maxVal - 1:
-        if distMap[xPos + 1, yPos] > map[xPos + 1, yPos] + distMap[xPos, yPos] and not visited[xPos + 1, yPos]:
-            distMap[xPos + 1, yPos] = map[xPos + 1, yPos] + distMap[xPos, yPos]
-            originMap[xPos + 1, yPos] = np.ravel_multi_index([xPos, yPos], (maxVal, maxVal))
+    def run(self):
+        width = self.gridWidth
+        height = self.gridHeight
+        map = np.random.randint(self.cellMin, self.cellMax, size=(width, height))
 
-    if xPos > 0:
-        if distMap[xPos - 1, yPos] > map[xPos - 1, yPos] + distMap[xPos, yPos] and not visited[xPos - 1, yPos]:
-            distMap[xPos - 1, yPos] = map[xPos - 1, yPos] + distMap[xPos, yPos]
-            originMap[xPos - 1, yPos] = np.ravel_multi_index([xPos, yPos], (maxVal, maxVal))
+        # Auxiliary arrays and variables
+        path = []
+        distMap = np.ones((width, height), dtype=int) * np.Infinity
+        distMap[0, 0] = 0
+        originMap = np.ones((width, height), dtype=int) * np.nan
+        visited = np.zeros((width, height), dtype=bool)
+        finished = False
+        xPos = int(0)
+        yPos = int(0)
+        count = 0
+        sum = 0
+        mapTemp = map.astype(float)
 
-    if yPos < maxVal - 1:
-        if distMap[xPos, yPos + 1] > map[xPos, yPos + 1] + distMap[xPos, yPos] and not visited[xPos, yPos + 1]:
-            distMap[xPos, yPos + 1] = map[xPos, yPos + 1] + distMap[xPos, yPos]
-            originMap[xPos, yPos + 1] = np.ravel_multi_index([xPos, yPos], (maxVal, maxVal))
+        start = time.time()
+        # Loop until reaching the target cell
+        while not finished:
+            # Checks the adjacent nodes to the current one and moves to the smallest in the direction of the goal.
+            if xPos > 0:
+                if distMap[xPos - 1, yPos] > map[xPos - 1, yPos] + distMap[xPos, yPos] and not visited[xPos - 1, yPos]:
+                    distMap[xPos - 1, yPos] = map[xPos - 1, yPos] + distMap[xPos, yPos]
+                    originMap[xPos - 1, yPos] = np.ravel_multi_index([xPos, yPos], (width, height))
 
-    if yPos > 0:
-        if distMap[xPos, yPos - 1] > map[xPos, yPos - 1] + distMap[xPos, yPos] and not visited[xPos, yPos - 1]:
-            distMap[xPos, yPos - 1] = map[xPos, yPos - 1] + distMap[xPos, yPos]
-            originMap[xPos, yPos - 1] = np.ravel_multi_index([xPos, yPos], (maxVal, maxVal))
+            if xPos < width - 1:
+                if distMap[xPos + 1, yPos] > map[xPos + 1, yPos] + distMap[xPos, yPos] and not visited[xPos + 1, yPos]:
+                    distMap[xPos + 1, yPos] = map[xPos + 1, yPos] + distMap[xPos, yPos]
+                    originMap[xPos + 1, yPos] = np.ravel_multi_index([xPos, yPos], (width, height))
 
-    visited[xPos, yPos] = True
-    distMapTemp = distMap
-    distMapTemp[np.where(visited)] = np.Infinity
+            if yPos > 0:
+                if distMap[xPos, yPos - 1] > map[xPos, yPos - 1] + distMap[xPos, yPos] and not visited[xPos, yPos - 1]:
+                    distMap[xPos, yPos - 1] = map[xPos, yPos - 1] + distMap[xPos, yPos]
+                    originMap[xPos, yPos - 1] = np.ravel_multi_index([xPos, yPos], (width, height))
 
-    # Find the shortest path
-    minPath = np.unravel_index(np.argmin(distMapTemp), np.shape(distMapTemp))
-    xPos, yPos = minPath[0], minPath[1]
-    if xPos == maxVal - 1 and yPos == maxVal - 1:
-        finished = True
-    count = count + 1
+            if yPos < height - 1:
+                if distMap[xPos, yPos + 1] > map[xPos, yPos + 1] + distMap[xPos, yPos] and not visited[xPos, yPos + 1]:
+                    distMap[xPos, yPos + 1] = map[xPos, yPos + 1] + distMap[xPos, yPos]
+                    originMap[xPos, yPos + 1] = np.ravel_multi_index([xPos, yPos], (width, height))
 
-# Auxiliary for backtracking
-mapTemp = map.astype(float)
-xPos, yPos = maxVal - 1, maxVal - 1
-path = []
-mapTemp[int(xPos), int(yPos)] = np.nan
+            visited[xPos, yPos] = True
+            distMapTemp = distMap
+            distMapTemp[np.where(visited)] = np.Infinity
 
-# Backtrack to plot path
-while xPos > 0.0 or yPos > 0.0:
-    path.append([int(xPos), int(yPos)])
-    xxyy = np.unravel_index(int(originMap[int(xPos), int(yPos)]), (maxVal, maxVal))
-    xPos, yPos = xxyy[0], xxyy[1]
-    mapTemp[int(xPos), int(yPos)] = np.nan
-    path.append([int(xPos), int(yPos)])
+            # Find the shortest path
+            minPath = np.unravel_index(np.argmin(distMapTemp), np.shape(distMapTemp))
+            xPos = minPath[0]
+            yPos = minPath[1]
+            if xPos == width - 1 and yPos == height - 1:
+                finished = True
+            count = count + 1
 
-# Visualize and output
-cMap = mpl.cm.get_cmap("binary").copy()  # I get an error without mpl....copy()
-cMap.set_bad(color='purple')
-fig, plot = plt.subplots(figsize=(10, 10))
-plot.matshow(mapTemp, cmap=cMap, vmin=0, vmax=15)
-for i in range(maxVal):
-    for j in range(maxVal):
-        c = map[j, i]
-        plot.text(i, j, str(c), va='center', ha='center')
+        end = time.time()
+        total = end - start
 
-# Display sum of path and show figure.
-print('Path Length = ' + str(distMap[maxVal - 1, maxVal - 1]))
-plt.show()
+        # Auxiliary for backtracking
+        xPos = width - 1
+        yPos = height - 1
+        mapTemp[int(xPos), int(yPos)] = np.nan
+
+        # Backtrack to plot path
+        while xPos > 0.0 or yPos > 0.0:
+            path.append([int(xPos), int(yPos)])
+            xxyy = np.unravel_index(int(originMap[int(xPos), int(yPos)]), (width, height))
+            xPos, yPos = xxyy[0], xxyy[1]
+            mapTemp[int(xPos), int(yPos)] = np.nan
+            path.append([int(xPos), int(yPos)])
+
+        # Visualize and output
+        cMap = mpl.cm.get_cmap("binary").copy()  # I get an error without mpl....copy()
+        cMap.set_bad(color='purple')
+        fig, plot = plt.subplots(figsize=(width, height))
+        plot.matshow(mapTemp, cmap=cMap, vmin=0, vmax=15)
+        for i in range(height):
+            for j in range(width):
+                c = map[j, i]
+                plot.text(i, j, str(c), va='center', ha='center')
+                sum = sum + map[j, i]
+
+
+        # Total Dimensions and sum
+        print('Dimensions Total = ' + str(width * height))
+        print('Sum of all cells = ' + str(sum))
+        # Display sum of path and show figure.
+        print('Path Length = ' + str(distMap[width - 1, height - 1]))
+        print('Took ' + str(total) + ' seconds')
+        plt.show()
